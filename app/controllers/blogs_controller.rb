fb2524ca812d471b03dc471c1f09047f4009de51
kuramochi2ndwave <kuramochi@2ndwave.jp>
2020-10-23 18:56:28 +0900

    20201023_18:56

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,50 @@
+class BlogsController < ApplicationController
+  def index
+     @blogs = Blog.all.order(created_at: "DESC")
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+   if @blog.save
+    redirect_to action: :show, id: @blog.id
+    flash[:success] = "記事の登録に成功しました"
+   else
+    flash[:danger] = "記事の登録に失敗しました"
+    render :new
+   end
+  end
+
+  def show
+      @blog = Blog.find(params[:id])
+  end
+
+  def edit
+      @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+   if @blog.update_attributes(blog_params)
+    flash[:success] = "記事の更新に成功しました"
+    redirect_to action: :show, id: @blog.id
+   else
+    flash[:danger] = "記事の更新に失敗しました"
+    render :edit
+   end
+  end
+
+  def destroy
+  Blog.find(params[:id]).destroy
+   flash[:success] = "記事を削除しました"
+   redirect_to blogs_path
+  end
+
+  private
+   def blog_params
+    params.require(:blog).permit(:id,:title,:content)
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/new'
+  get 'blogs/create'
+  get 'blogs/show'
+  get 'blogs/edit'
+  get 'blogs/update'
+  get 'blogs/destroy'
+  resources :blogs
+  root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
